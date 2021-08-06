9a59ef261da2c4fd7be2f93e5fea56712d3b266d
Nabuchadresar <engmec.andremoura@gmail.com>
2021-08-06 00:41:04 -0300

    first commit

--- PythonCalculator.py
+++ PythonCalculator.py
@@ -24,4 +24,4 @@ if 1 <= x <= 4:
     print("A sua operação é:")
     print(f"{a} {operador} {b} = {round(c, 2)}")
 else:
-    print('Opção inválida')
+    print('Opção inválida')# datascienceacademystudies
